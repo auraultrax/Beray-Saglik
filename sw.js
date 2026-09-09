64900124aa000bdedcd28ae41969b2726f20aebd
auraultrax <xauraultra@gmail.com>
2026-09-09 13:20:47 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE='beray-health-20m-v1';
-const ASSETS=['./','./index.html','./app.js','./healthData.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',event=>{
-  if(event.request.method!=='GET') return;
-  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return response;}).catch(()=>caches.match('./index.html'))));
-});
